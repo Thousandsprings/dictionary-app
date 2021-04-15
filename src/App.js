2fdfacc057dc7a83172f2f285bd13d863c0608f7
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2021-04-15 19:14:07 +0900

    img delete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
-      <img src="https://www.shecodes.io/assets/branding/logo-shecodes-f9fa0540d113c086f61eb6e89466c0cbd24a42163b6a96d4b01da078803f53ee.png" />
+      {/* <img
+        alt="SheCodes"
+        src="https://www.shecodes.io/assets/branding/logo-shecodes-f9fa0540d113c086f61eb6e89466c0cbd24a42163b6a96d4b01da078803f53ee.png"
+      /> */}
       <Dictionary />
     </div>
   );
