a130cee6a77b7621a1b42f5007dc19b9df3dd01c
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2021-04-17 22:27:36 +0900

    imported axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
-  function handleSubmit() {
-    alert(`Searching for "${keyword}"`);
+  function handleResult(response) {
+    console.log(response.data[0]);
+    console.log(response.data[0].word);
   }
 
-  function handleSearch(event) {
+  function handleSubmit(event) {
+    event.preventDefault(); //👈important!!
+    // alert(`Searching for "${keyword}"`);
+
+    //https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResult);
+  }
+
+  function handleKeywordChange(event) {
     // console.log(event.target.value);
     setKeyword(event.target.value);
   }
@@ -19,7 +30,7 @@ export default function Dictionary() {
           className="shadow"
           type="search"
           autoFocus={true}
-          onChange={handleSearch}
+          onChange={handleKeywordChange}
         ></input>
       </form>
     </div>
